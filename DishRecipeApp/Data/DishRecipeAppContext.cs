035decfc29308c8bf131dfccd06ee9e928287be6
Virginia Rosianu <gina.rosianu@gmail.com>
2020-06-30 01:33:55 +0300

    Add project files.

---
+++ DishRecipeApp/Data/DishRecipeAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DishRecipeApp.Models;
+
+namespace DishRecipeApp.Data
+{
+    public class DishRecipeAppContext : DbContext
+    {
+        public DishRecipeAppContext (DbContextOptions<DishRecipeAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DishRecipeApp.Models.Dish> Dish { get; set; }
+    }
+}
---
+++ DishRecipeApp/Models/DishesDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DishRecipeApp.Models
+{
+    public class DishesDbContext : IdentityDbContext
+    {
+        public DishesDbContext(DbContextOptions<DishesDbContext> options) : base(options) {}
+
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        
+        
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    //modelBuilder.Entity<Expense>()
+        //    //   .HasMany(c => c.Comments)
+        //    //   .WithOne(e => e.Expense)
+        //    //   .OnDelete(DeleteBehavior.Cascade);
+
+        //    modelBuilder.Entity<Dish>()
+        //        .HasMany(r => r.Reviews)
+        //        .WithOne(d => d.Dish)
+        //        .OnDelete(DeleteBehavior.Cascade);
+
+        //    modelBuilder.Entity<Ingredient>()
+        //       .HasMany(d => d.Dishes)
+        //       .WithOne(i => i.Ingredient)
+        //       .OnDelete(DeleteBehavior.Cascade);
+        //}
+    }
+}
